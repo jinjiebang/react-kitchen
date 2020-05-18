eddbe5d76d1c0bd2ddbe84c1fb22ad50ca16af54
jinjie.wang <724895582@qq.com>
2020-05-18 14:42:53 +0800

    新增菜肴列表

---
+++ src/components/DishesList/index.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+class DishesList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div></div> );
+    }
+}
+ 
+export default DishesList;
